02bb4882323c7accf00e99e54b219f9ed58fd394
santosh <kumarpradhan.santosh@gmail.com>
2021-02-27 21:09:58 +0530

    first commit

---
+++ src/main/java/qaclickacademy/MavenHome/App.java
@@ -0,0 +1,13 @@
+package qaclickacademy.MavenHome;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/AndroidTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy.MavenHome;
+
+import org.testng.annotations.Test;
+
+public class AndroidTest {
+	
+	@Test
+	public void browserauthenrestandroid()
+	{
+		
+		System.out.println("I am printing android 1st block");
+	}
+	
+	@Test
+	public void UiElementsrestandroid()
+	{
+		
+		System.out.println("I am printing android 2nd block");
+
+}
+}
